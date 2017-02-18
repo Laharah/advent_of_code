052a7250031d6f963025e51f2f04713b7840863e
laharah <laharah22@gmail.com>
2017-02-18 11:58:43 -0800

    created a faster (and accurate) recursive version of day 17

--- day17.py
+++ day17.py
@@ -1,5 +1,6 @@
 from common import input
-import itertools
+from collections import Counter
+from my_utils.decorators import memo
 
 
 def parse(infile):
@@ -8,31 +9,30 @@ def parse(infile):
 
 coins = parse(input(17))
 target = 150
-total = 0
-fewest = len(coins)
-num_fewest = 0
 
-for mask in range(1, 1 << len(coins)):
-    mask = '{:>0{lc}}'.format(bin(mask)[2:], lc=len(coins))
-    if sum(itertools.compress(coins, (int(c) for c in mask))) == target:
-        num_containers = sum(int(c) for c in mask)
-        if num_containers < fewest:
-            num_fewest = 1
-            fewest = num_containers
-        elif num_containers == fewest:
-            num_fewest += 1
-        total += 1
+#test data
+# coins = 20, 10, 15, 5, 5
+# target = 25
 
-def recursive_solution(coins, target):
+depths = Counter()
+
+
+def recursive_solution(coinsleft, target, depth=0):
     if target == 0:
+        depths[depth] += 1
         return 1
     if target < 0:
         return 0
 
     solutions = 0
-    for coin in coins:
-        solutions += recursive_solution(coins-{coin}, target-coin)
+    used = set()
+    for coin in coinsleft:
+        used.add(coin)
+        solutions += recursive_solution(coinsleft - used, target - coin[1], depth + 1)
     return solutions
 
-print(total, fewest, num_fewest)
-print(recursive_solution(set(coins), target))
+
+# we add an identifyer to each coin so duplicate coin values are allowed in a
+# single stack
+print(recursive_solution(frozenset((i, v) for i, v in enumerate(coins)), target))
+print(depths)
