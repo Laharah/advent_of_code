51c2f56e105198da850f13367011f43424036d3f
laharah <laharah22@gmail.com>
2017-12-01 21:21:09 -0800

    Day 02 done

---
+++ 2017/day02.py
@@ -0,0 +1,27 @@
+from common import Input
+import itertools
+
+matrix = []
+
+for line in Input(2):
+    line = line.strip().split()
+    matrix.append([int(x) for x in line])
+
+total = 0
+
+for line in matrix:
+    total += max(line) - min(line)
+print(total)
+
+
+total = 0
+for row in matrix:
+    for a, b in itertools.combinations(row, 2):
+        if a % b == 0:
+            total += a // b
+            break
+        elif b % a == 0:
+            total += b // a
+            break
+
+print(total)
