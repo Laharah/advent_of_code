d93cd60490e5994cf02f7ff8b4eba86770832ac9
laharah <laharah22@gmail.com>
2017-12-03 21:26:44 -0800

    refactored for efficiency

--- 2017/day04.py
+++ 2017/day04.py
@@ -3,12 +3,10 @@ from common import Input
 total = 0
 for line in Input(4):
     words = set()
-    for word in line.strip().split():
-        sword = ''.join(sorted(word))
-        if word in words or sword in words:
+    for word in (''.join(sorted(c for c in w)) for w in line.strip().split()):
+        if word in words:
             break
         words.add(word)
-        words.add(sword)
     else:
         total += 1
 print(total)
