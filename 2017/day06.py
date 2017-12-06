bea96a77b475d8f2b8faee74ac3aa6cbf10e9291
laharah <laharah22@gmail.com>
2017-12-06 12:04:59 -0800

    refactored day06 for cleanliness

--- 2017/day06.py
+++ 2017/day06.py
@@ -1,39 +1,35 @@
 from common import Input
 import itertools
 
-mem = [int(x) for x in Input(6).read().split()]
-states = {tuple(mem)}
-blocks = len(mem)
 
-for c in itertools.count(1):
-    i = max(range(blocks), key=lambda x: mem[x])
-    items = mem[i]
-    mem[i] = 0
-    while items:
-        i = (i + 1) % blocks
-        mem[i] += 1
-        items -= 1
-    state = tuple(mem)
-    if state in states:
-        break
-    states.add(state)
+def distribute_cycle(mem):
+    blocks = len(mem)
+    while True:
+        i = max(range(blocks), key=lambda x: mem[x])
+        items = mem[i]
+        mem[i] = 0
+        while items:
+            i = (i + 1) % blocks
+            mem[i] += 1
+            items -= 1
+        yield tuple(mem)
 
-print(c)
 
-goal = tuple(mem)
+if __name__ == '__main__':
 
-for c in itertools.count(1):
-    i = max(range(blocks), key=lambda x: mem[x])
-    items = mem[i]
-    mem[i] = 0
-    while items:
-        i = (i + 1) % blocks
-        mem[i] += 1
-        items -= 1
-    state = tuple(mem)
-    if state == goal:
-        break
+    mem = [int(x) for x in Input(6).read().split()]
+    states = {tuple(mem)}
 
-print(c)
+    for c, state in enumerate(distribute_cycle(mem), 1):
+        if state in states:
+            print(c)
+            break
+        states.add(state)
 
+
+    goal = tuple(mem)
+    for c, state in enumerate(distribute_cycle(mem), 1):
+        if state == goal:
+            print(c)
+            break
 
