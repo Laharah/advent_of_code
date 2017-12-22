934c499671793e950674d31a1332edd0f5ad42e7
laharah <laharah22@gmail.com>
2017-12-22 02:09:44 -0800

    finished day 22, infinite grid worm

--- 2017/day22.py
+++ 2017/day22.py
@@ -1,13 +1,14 @@
-from collections import defaultdict
 from common import Input
 
+
 def make_grid(Input):
-    grid = defaultdict(bool)
+    grid = {}
     for r, line in enumerate(Input):
         for c, infected in enumerate(line.strip()):
-            grid[(r,c)] = infected == '#'
-    r_center = r//2 + r%2
-    c_center = c//2 + c%2
+            if infected == '#':
+                grid[(r, c)] = 'i'
+    r_center = r // 2 + r % 2
+    c_center = c // 2 + c % 2
     return grid, (r_center, c_center)
 
 
@@ -15,24 +16,33 @@ def burst(pos, grid):
     dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     current_dir = 0
     while True:
-        current_dir = (current_dir + 1)%4 if grid[pos] else (current_dir - 1)%4
+        try:
+            current_sq = grid[pos]
+        except KeyError:
+            current_sq = 'c'
+
+        if current_sq == 'c':
+            current_dir = (current_dir - 1) % 4
+            grid[pos] = 'w'
+        elif current_sq == 'w':
+            grid[pos] = 'i'
+        elif current_sq == 'i':
+            current_dir = (current_dir + 1) % 4
+            grid[pos] = 'f'
+        elif current_sq == 'f':
+            current_dir = (current_dir + 2) % 4
+            del grid[pos]
+
+        yield current_sq
         dr, dc = dirs[current_dir]
         r, c = pos
-        if not grid[pos]:
-            yield True
-            grid[pos] = True
-        else:
-            yield False
-            grid[pos] = False
-        pos = (r+dr, c+dc)
+        pos = (r + dr, c + dc)
+
 
 if __name__ == '__main__':
     grid, center = make_grid(Input(22))
     flips = 0
     g = burst(center, grid)
-    for _ in range(10000):
-        flips += next(g)
+    for _ in range(10000000):
+        flips += next(g) == 'w'
     print(flips)
-
-
-
