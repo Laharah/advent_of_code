32dc14a6926d9fc65b3ab3cb5afddfb39042f532
laharah <laharah22@gmail.com>
2017-12-26 12:10:50 -0800

    formatting

--- 2017/day23.py
+++ 2017/day23.py
@@ -69,8 +69,8 @@ def execute(instructions):
 if __name__ == '__main__':
     print(execute(Input(23)))
     h = 0
-    for x in range(108100,125100 + 1,17):
-        for i in range(2,x):
+    for x in range(108100, 125100 + 1, 17):
+        for i in range(2, x):
             if x % i == 0:
                 h += 1
                 break
